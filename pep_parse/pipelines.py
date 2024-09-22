4625ef767a152642e91f5d3177d9ac2094597774
Александр <sasatoguzov@gmail.com>
2024-09-22 13:42:35 +0300

    CODE

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,17 +1,15 @@
 import csv
 import time
 from collections import defaultdict
-from pathlib import Path
 
 from .settings import BASE_DIR, RESULT_DIR
 
 
 class PepParsePipeline:
-    def __init__(self):
-        self.result_dir = Path(BASE_DIR / RESULT_DIR)
-        self.result_dir.mkdir(exist_ok=True)
 
     def open_spider(self, spider):
+        self.results = BASE_DIR / RESULT_DIR
+        self.results.mkdir(exist_ok=True)
         self.status_count = defaultdict(int)
 
     def process_item(self, item, spider):
@@ -20,7 +18,7 @@ class PepParsePipeline:
 
     def close_spider(self, spider):
         timestamp = time.strftime('%Y-%m-%dT%H-%M-%S')
-        file_path = self.result_dir / f'status_summary_{timestamp}.csv'
+        file_path = self.results / f'status_summary_{timestamp}.csv'
         rows = [(status, count) for status, count in self.status_count.items()]
         rows.append(('Total', sum(self.status_count.values())))
         with open(file_path, mode='w', encoding='utf-8', newline='') as f:
